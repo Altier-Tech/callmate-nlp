b8dfb588d982857e8f6192ac4f980ba9f1c3427e
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-10-14 04:03:09 +0530

    Update parser.py

--- val_engine/parser.py
+++ val_engine/parser.py
@@ -7,7 +7,7 @@ from dateutil import parser
 nlp = spacy.load("en_core_web_sm")
 
 
-# Function to extract date and time from input string
+# Function to extract date, time, and day of the week from input string
 def extract_date_time(input_string):
     # Process the input string using spaCy
     doc = nlp(input_string)
@@ -15,29 +15,33 @@ def extract_date_time(input_string):
     # Get the current date and time
     current_datetime = datetime.now()
 
-    # Initialize variables to store date and time
+    # Initialize variables to store date, time, and day of the week
     extracted_date = None
     extracted_time = None
+    extracted_day = None
 
-    # Extract entities (including date and time) using spaCy's NER
+    # Extract entities (including date, time, and day of the week) using spaCy's NER
     for ent in doc.ents:
         if ent.label_ == "DATE":
             # Parse the date using dateutil.parser
             extracted_date, tokens = parser.parse(ent.text, fuzzy_with_tokens=True)
+            # Extract the day of the week from the parsed date
+            extracted_day = extracted_date.strftime("%A")
         elif ent.label_ == "TIME":
             # Parse the time using dateutil.parser
             extracted_time, tokens = parser.parse(ent.text, fuzzy_with_tokens=True)
 
-    # Return the extracted date and time
-    return extracted_date, extracted_time
+    # Return the extracted date, time, and day of the week
+    return extracted_date, extracted_time, extracted_day
 
 
 # Example input string
 input_str = "Hey, I have a meeting this thursday at 10 am."
 
-# Extract date and time from the input string
-ext_date, ext_time = extract_date_time(input_str)
+# Extract date, time, and day of the week from input string
+extracted_date, extracted_time, extracted_day = extract_date_time(input_str)
 
-# Print the extracted date and time
-print("Extracted Date:", ext_date)
-print("Extracted Time:", ext_time)
+# Print the extracted date, time, and day of the week
+print("Extracted date:", extracted_date)
+print("Extracted time:", extracted_time)
+print("Extracted day:", extracted_day)
