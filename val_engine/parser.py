0a54b6870f99433707cdf761050240fa9ae6306b
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-10-14 03:51:33 +0530

    Create parser.py

---
+++ val_engine/parser.py
@@ -0,0 +1,37 @@
+import spacy
+from dateutil import parser
+
+# Load the English language model from spaCy
+nlp = spacy.load("en_core_web_sm")
+
+
+# Function to extract date and time from input string
+def extract_date_time(input_string):
+    # Process the input string using spaCy
+    doc = nlp(input_string)
+
+    # Initialize variables to store date and time
+    extracted_date = None
+    extracted_time = None
+
+    # Extract entities (including date and time) using spaCy's NER
+    for ent in doc.ents:
+        if ent.label_ == "DATE":
+            # Parse the date using dateutil.parser
+            extracted_date = parser.parse(ent.text).date()
+        elif ent.label_ == "TIME":
+            # Parse the time using dateutil.parser
+            extracted_time = parser.parse(ent.text).time()
+
+    return extracted_date, extracted_time
+
+
+# Example input string
+input_string = "Hey, I have a meeting tomorrow at 10 am."
+
+# Extract date and time from the input string
+extracted_date, extracted_time = extract_date_time(input_string)
+
+# Print the extracted date and time
+print("Extracted Date:", extracted_date)
+print("Extracted Time:", extracted_time)
