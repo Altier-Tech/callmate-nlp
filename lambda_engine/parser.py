473d3ea97438b71c65c145ae0b38cf37482df4e6
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-10-14 03:45:34 +0530

    Update parser.py

--- lambda_engine/parser.py
+++ lambda_engine/parser.py
@@ -10,7 +10,7 @@ with open("../bin/lambda_model.pkl", "rb") as f:
 
 while True:
     # Example input sentence to predict action
-    input_sentence = input("Say: ")  # "Remind me to buy groceries tomorrow"
+    input_sentence = input("Say (or q to exit): ")  # "Remind me to buy groceries tomorrow"
 
     if input_sentence == "q":
         exit()
