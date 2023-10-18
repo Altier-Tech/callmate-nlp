4c76790347cdbb0cfb6dcc6bbd6bc5efb3913784
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-10-18 14:48:40 +0530

    Update chat.py

--- open_ai/chat.py
+++ open_ai/chat.py
@@ -6,16 +6,17 @@ conversation_history = [
     {"role": "system", "content": "You are a helpful assistant."},
 ]
 
+
 while True:
     user_input = input("User: ")
     if user_input.lower() in ['exit', 'quit', 'bye']:
         break
 
+    # Add user's message to the conversation history
     conversation_history.append({"role": "user", "content": user_input})
 
     # Format conversation history as a single string
-    messages = [{"role": role, "content": content} for role, content in conversation_history]
-    chat_input = "\n".join(f"{msg['role']}: {msg['content']}" for msg in messages)
+    chat_input = "\n".join(f"{msg['role']}: {msg['content']}" for msg in conversation_history)
 
     # Generate a response from the AI model
     response = openai.Completion.create(
@@ -23,8 +24,6 @@ while True:
         prompt=chat_input,
         max_tokens=150  # You can adjust the response length as needed
     )
-
-    print(response)
 
     # Extract and print the assistant's reply
     assistant_reply = response.choices[0].text.strip()
