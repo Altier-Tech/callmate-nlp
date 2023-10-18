bc2f91f1c7330e164134c06e6bfb8775d0206889
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-10-18 14:49:35 +0530

    Update chat.py

--- open_ai/chat.py
+++ open_ai/chat.py
@@ -6,8 +6,26 @@ conversation_history = [
     {"role": "system", "content": "You are a helpful assistant."},
 ]
 
+async def chat(input: str):
+# Add user's message to the conversation history
+conversation_history.append({"role": "user", "content": user_input})
 
+# Format conversation history as a single string
+chat_input = "\n".join(f"{msg['role']}: {msg['content']}" for msg in conversation_history)
 
+# Generate a response from the AI model
+response = openai.Completion.create(
+    engine="text-davinci-003",
+    prompt=chat_input,
+    max_tokens=150  # You can adjust the response length as needed
+)
+
+# Extract and print the assistant's reply
+assistant_reply = response.choices[0].text.strip()
+print(f"ChatGPT: {assistant_reply}")
+
+# Store the assistant's reply in the conversation history
+conversation_history.append({"role": "assistant", "content": assistant_reply})
 
 
 if __name__ == '__main__':
