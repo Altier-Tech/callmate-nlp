c722245fb0ec2e36ad255a10770c345f7a42c519
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-10-18 14:48:58 +0530

    Update chat.py

--- open_ai/chat.py
+++ open_ai/chat.py
@@ -8,27 +8,27 @@ conversation_history = [
 
 
 if __name__ == '__main__':
-while True:
-    user_input = input("User: ")
-    if user_input.lower() in ['exit', 'quit', 'bye']:
-        break
+    while True:
+        user_input = input("User: ")
+        if user_input.lower() in ['exit', 'quit', 'bye']:
+            break
 
-    # Add user's message to the conversation history
-    conversation_history.append({"role": "user", "content": user_input})
+        # Add user's message to the conversation history
+        conversation_history.append({"role": "user", "content": user_input})
 
-    # Format conversation history as a single string
-    chat_input = "\n".join(f"{msg['role']}: {msg['content']}" for msg in conversation_history)
+        # Format conversation history as a single string
+        chat_input = "\n".join(f"{msg['role']}: {msg['content']}" for msg in conversation_history)
 
-    # Generate a response from the AI model
-    response = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=chat_input,
-        max_tokens=150  # You can adjust the response length as needed
-    )
+        # Generate a response from the AI model
+        response = openai.Completion.create(
+            engine="text-davinci-003",
+            prompt=chat_input,
+            max_tokens=150  # You can adjust the response length as needed
+        )
 
-    # Extract and print the assistant's reply
-    assistant_reply = response.choices[0].text.strip()
-    print(f"ChatGPT: {assistant_reply}")
+        # Extract and print the assistant's reply
+        assistant_reply = response.choices[0].text.strip()
+        print(f"ChatGPT: {assistant_reply}")
 
-    # Store the assistant's reply in the conversation history
-    conversation_history.append({"role": "assistant", "content": assistant_reply})
+        # Store the assistant's reply in the conversation history
+        conversation_history.append({"role": "assistant", "content": assistant_reply})
