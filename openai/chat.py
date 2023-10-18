44e6d1948e3a94acce2f21e34c64ff04614dac0c
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-10-18 14:33:59 +0530

    Update chat.py

--- openai/chat.py
+++ openai/chat.py
@@ -3,6 +3,7 @@ import openai
 openai.api_key = 'YOUR_API_KEY'
 messages = [{"role": "system", "content":
              "You are a intelligent assistant."}]
+
 while True:
     message = input("User : ")
     if message:
