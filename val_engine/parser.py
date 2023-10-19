c133247f0ed2d35969e1e62d78dcbd588838729b
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-10-19 14:29:52 +0530

    Update parser.py

--- val_engine/parser.py
+++ val_engine/parser.py
@@ -7,39 +7,42 @@ import parsedatetime
 nlp = spacy.load("en_core_web_sm")
 
 
+# Initialize parsedatetime.Calendar() instance
+cal = parsedatetime.Calendar()
+
 # Function to extract date, time, and day of the week from input string
 def extract_date_time(input_string):
     # Process the input string using spaCy
     doc = nlp(input_string)
 
-    # Get the current date and time
-    current_datetime = datetime.now()
-
     # Initialize variables to store date, time, and day of the week
     extracted_date = None
     extracted_time = None
     extracted_day = None
-    
-    cal = parsedatetime.Calendar()
 
     # Extract entities (including date, time, and day of the week) using spaCy's NER
     for ent in doc.ents:
         if ent.label_ == "DATE":
-            # Parse the date using dateutil.parser
-            # extracted_date, tokens = parser.parse(ent.text, fuzzy_with_tokens=True)
-            extracted_date, tokens = cal.parse(ent.text)
+            # Parse the date using parsedatetime
+            time_struct, parse_status = cal.parse(ent.text)
+            # Convert time.struct_time to datetime object
+            extracted_date = datetime(*time_struct[:6])
             # Extract the day of the week from the parsed date
             extracted_day = extracted_date.strftime("%A")
         elif ent.label_ == "TIME":
-            # Parse the time using dateutil.parser
-            extracted_time, tokens = parser.parse(ent.text, fuzzy_with_tokens=True)
+            # Parse the time using parsedatetime
+            time_struct, parse_status = cal.parse(ent.text)
+            # Convert time.struct_time to datetime object
+            extracted_time = datetime(*time_struct[:6])
+            # Format the time in 24-hour format
+            extracted_time = extracted_time.strftime("%H:%M:%S")
 
     # Return the extracted date, time, and day of the week
     return extracted_date, extracted_time, extracted_day
 
 
 # Example input string
-input_str = "Hey, I have a meeting today at 10 am."
+input_str = "Hey, I have a meeting next friday at 10 am."
 
 # Extract date, time, and day of the week from input string
 ext_date, ext_time, ext_day = extract_date_time(input_str)
